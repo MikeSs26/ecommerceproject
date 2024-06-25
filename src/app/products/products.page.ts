6abab4ea4c3bb02dc98917a6409e301100c77406
jazjv04 <jazminednaly.jefa.jfa@gmail.com>
2024-06-25 15:22:34 -0500

    Parte logica

--- src/app/products/products.page.ts
+++ src/app/products/products.page.ts
@@ -3,6 +3,16 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonIcon } from '@ionic/angular/standalone';
 
+interface Product {
+  title: string;
+  price: number;
+  image: string;
+}
+
+interface CartItem extends Product {
+  quantity: number;
+}
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.page.html',
@@ -19,7 +29,54 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonIcon } from 
     IonIcon,
   ]
 })
-export class ProductsPage {
 
-  constructor() { }
+  export class ProductsPage {
+    products: Product[] = [
+      { title: 'Articulo 1', price: 150.00, image: 'assets/images/carrito.png' },
+      { title: 'Articulo 2', price: 2500.00, image: 'assets/images/carrito.png' },
+      // Agrega el resto de productos aquí
+    ];
+  
+    cartItems: CartItem[] = [];
+  
+    addToCart(product: Product) {
+      const existingItem = this.cartItems.find(item => item.title === product.title);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        this.cartItems.push({ ...product, quantity: 1 });
+      }
+    }
+  
+    removeFromCart(item: CartItem) {
+      this.cartItems = this.cartItems.filter(cartItem => cartItem.title !== item.title);
+    }
+  
+    increaseQuantity(item: CartItem) {
+      item.quantity++;
+    }
+  
+    decreaseQuantity(item: CartItem) {
+      if (item.quantity > 1) {
+        item.quantity--;
+      } else {
+        this.removeFromCart(item);
+      }
+    }
+  
+    getTotalPrice() {
+      return this.cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+    }
+  
+    checkout() {
+      alert("Gracias por la compra");
+      this.cartItems = [];
+    }
+
+    handleKeyDown(event: KeyboardEvent, action: Function, ...args: any[]) {
+      if (event.key === 'Enter' || event.key === ' ') {
+        event.preventDefault();
+        action(...args);
+      }
+  }
 }
