212133d58929b5e27db2dd7ad3baee56ddc1e3a3
JacquesMartinez <jck.alexander7@gmail.com>
2024-07-14 15:56:20 -0500

    products

--- src/app/product-detail/product-detail.page.ts
+++ src/app/product-detail/product-detail.page.ts
@@ -5,26 +5,30 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../service/product.service';
 import { CarritoService } from '../service/carrito.services';
-import axios  from 'axios';
+import { FooterComponent } from '../footer/footer.component';
+import axios from 'axios';
 
-
+interface ProductImage {
+  id_images: number;
+  url: string;
+  fk_product: number;
+}
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.page.html',
   styleUrls: ['./product-detail.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule],
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, 
+    FooterComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ProductDetailPage implements OnInit {
   productId: string;
   product: any; // Define el tipo de tu producto aquí
-  urlimages: any;
+  urlimages: ProductImage[] = [];
   seleccion: any;
   products: any;
-
-  
 
   constructor(private route: ActivatedRoute, private productService: ProductService, public CarritoService: CarritoService) {
     this.productId = ''; // Inicializar productId como string vacío
@@ -35,7 +39,7 @@ export class ProductDetailPage implements OnInit {
     if (productIdParam !== null) {
       this.productId = productIdParam;
       this.loadProductDetail();
-      this.loadProducImages();
+      this.fetchProductsImage(); // Asegúrate de llamar a este método para obtener las imágenes
     } else {
       console.error('Product ID is null');
       // Puedes manejar el caso donde productId es null, por ejemplo, redirigiendo o mostrando un mensaje de error
@@ -58,15 +62,20 @@ export class ProductDetailPage implements OnInit {
       action(...args);
     }
   }
-  loadProducImages() {
-    this.productService.getImageById(this.productId)
-      .then((image) => {
-        this.urlimages = image;
+
+  fetchProductsImage() {
+    return axios.get(`http://localhost:8080/api/productimages?fk_product=${this.productId}`)
+      .then(response => {
+        if (response.data) {
+          this.urlimages = response.data.map((image: any) => ({
+            url: image.url,
+          }));
+          return response.data; // Asegúrate de devolver los datos correctamente
+        }
       })
-      .catch((error) => {
-        console.error('Error fetching product details: ', error);
+      .catch(error => {
+        console.error(`Error fetching image for product ${this.productId}:`, error);
+        return null;
       });
   }
-
-  
 }
