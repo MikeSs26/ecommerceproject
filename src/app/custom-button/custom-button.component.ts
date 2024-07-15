9abd7ce76f77eb986964778b0e73cbc9538421fd
JacquesMartinez <jck.alexander7@gmail.com>
2024-07-14 21:17:23 -0500

    boton

---
+++ src/app/custom-button/custom-button.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-custom-button',
+  templateUrl: './custom-button.component.html',
+  styleUrls: ['./custom-button.component.scss'],
+  standalone: true,
+  imports: [CommonModule, IonicModule, FormsModule]
+})
+export class CustomButtonComponent {
+
+  @Output() clicked = new EventEmitter<void>();
+
+  onClick() {
+    this.clicked.emit();
+  }
+
+}
