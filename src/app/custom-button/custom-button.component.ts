76e6292b8f416eae9a0fcf89f2a3c060d64d4dc5
jazjv04 <jazminednaly.jefa.jfa@gmail.com>
2024-07-15 11:19:37 -0500

    Solucion Slider

--- src/app/custom-button/custom-button.component.ts
+++ src/app/custom-button/custom-button.component.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { Component, EventEmitter, Output } from '@angular/core';
-=======
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
->>>>>>> 309fe093048287bfdf1319551c1565502eb2ae99
+import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
