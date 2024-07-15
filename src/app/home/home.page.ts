db95667480828da8b41341831ece98bf23d58758
jazjv04 <jazminednaly.jefa.jfa@gmail.com>
2024-07-14 22:35:02 -0500

    Actualizacion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,25 +1,35 @@
 import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import { IonContent} from '@ionic/angular/standalone';
 import { HeaderComponent } from '../header/header.component';
 import { SelectComponent } from '../select/select.component';
 import { FondoComponent } from '../fondo/fondo.component';
 import { MenuComponent } from '../menu/menu.component';
 import { FooterComponent } from '../footer/footer.component';
 import { BannerComponent } from '../banner/banner.component';
+import { IonContent, IonGrid,IonRow,IonCol,IonCard,IonCardHeader,IonCardTitle,IonCardContent,} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [HeaderComponent, 
+  imports: [
+    HeaderComponent, 
     SelectComponent,
     IonContent, 
     FondoComponent,
     IonContent, 
     MenuComponent,
     FooterComponent, 
-    BannerComponent
+    BannerComponent,
+    FondoComponent,
+    FooterComponent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent
   ],
 })
 
